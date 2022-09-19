d344c55b33239a8d853ab2bb5f34803c5a316844
shadab <shadab14meb346@gmail.com>
2022-09-19 22:53:58 +0530

    run formatter

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,12 +1,12 @@
 import "@mui/material/styles/createPalette";
 
 declare module "@mui/material/styles" {
-	interface Palette {
-		grey1: Palette["primary"];
-	}
+  interface Palette {
+    grey1: Palette["primary"];
+  }
 
-	// allow configuration using `createTheme`
-	interface PaletteOptions {
-		grey1?: Palette["primary"];
-	}
+  // allow configuration using `createTheme`
+  interface PaletteOptions {
+    grey1?: Palette["primary"];
+  }
 }
